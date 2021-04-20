99a8392d75c23b330ad6ae6255e3b7fb1df517d0
Paul <62744170+PaulMoustakas@users.noreply.github.com>
2021-04-20 15:20:35 +0200

    Character

---
+++ Project/Assets/Bow.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//This is the script for the characters bows
+
+public class Bow : MonoBehaviour {
+
+    private Ray mouseRay;
+    private RaycastHit rayCastHit;
+
+    private float posX;     //Coordiante for the raycast on screen
+    private float posY; 
+
+
+    // Arrow object reference
+
+
+
+
+
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+
+    public void shootArrow () {
+
+
+    }
+}
