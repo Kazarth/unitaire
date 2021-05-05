91440735cf15a548d5e265519db9f25950309a31
Paul <62744170+PaulMoustakas@users.noreply.github.com>
2021-05-05 14:26:13 +0200

    yh

---
+++ Project/Assets/MovePlayer 2.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    Vector3 movementDirections;
+
+
+
+
+    public CharacterController controller;  // new
+
+  
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        
+if (Input.GetKey(KeyCode.UpArrow))
+    transform.position += transform.forward * Time.deltaTime * 5;
+
+if (Input.GetKey(KeyCode.DownArrow))
+    transform.position -= transform.forward * Time.deltaTime * 2;
+
+
+if (Input.GetKey(KeyCode.LeftArrow))
+    transform.Rotate(new Vector3(0, -10, 0) * Time.deltaTime * 20);
+
+if (Input.GetKey(KeyCode.RightArrow))
+    transform.Rotate(new Vector3(0, +10, 0) * Time.deltaTime * 20);
+
+
+    }
+}
+
+
+
+
+
+
+
+// transform.position += transform.forward * Time.deltaTime * 20; 
+
+
+//   movementDirections.z = Input.GetAxis("left") * Time.deltaTime * 20;
+//        float horizontalInput = Input.GetAxis("Vertical") * Time.deltaTime * 20;
+//      float zInput =  Input.GetAxis("Vertical") * Time.deltaTime * 20;
+
+//  float verticalInput   = Input.GetAxis("Vertical") * Time.deltaTime * 20;
+
+
+//      transform.position = transform.position + new Vector3 (horizontalInput, zInput); 
