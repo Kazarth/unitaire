40af9a78f96c4dd5f9c5befb7d86246a29a54181
viktorpolak98 <61971346+viktorpolak98@users.noreply.github.com>
2021-05-19 16:32:46 +0200

    Update CameraMovement.cs

--- Project/Assets/Scripts/CameraMovement.cs
+++ Project/Assets/Scripts/CameraMovement.cs
@@ -7,7 +7,6 @@ public class CameraMovement : MonoBehaviour
     float yaw = 0;
     float pitch = 0;
 
- 
     void Start()
     {
 
@@ -15,6 +14,7 @@ public class CameraMovement : MonoBehaviour
  
     void Update()
     {   
+  
         yaw -= 2 * Input.GetAxis("Mouse X");
         pitch += 2 * Input.GetAxis("Mouse Y");
         yaw = Mathf.Clamp(yaw, 69f, 90f);
@@ -22,6 +22,6 @@ public class CameraMovement : MonoBehaviour
 
         if(Input.GetMouseButton(1)){
                 transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
-            }
+        }
     }
 }
