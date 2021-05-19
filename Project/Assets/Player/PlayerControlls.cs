692b42e9caa72ea217b6e2e9946085f2de42884b
Paul <62744170+PaulMoustakas@users.noreply.github.com>
2021-05-19 12:03:26 +0200

    Update
    Movement and release

--- Project/Assets/Player/PlayerControlls.cs
+++ Project/Assets/Player/PlayerControlls.cs
@@ -7,14 +7,14 @@ public class PlayerControlls : MonoBehaviour
 
 
     static Animator anim;
-    public float speed = 0; 
+    public float speed = 0;
     public float rotationSpeed = 100.0f;
 
 
 
     private float timeRemaining = 3;
     private bool isBowDrawn;
-   
+
 
 
 
@@ -23,23 +23,15 @@ public class PlayerControlls : MonoBehaviour
     void Start()
     {
         anim = GetComponent<Animator>();
-        isBowDrawn = false; 
-        
+        isBowDrawn = false;
     }
 
-  
+
     void Update()
     {
 
-        float translation = Input.GetAxis("Vertical") * speed;
-       // print(translation);
-        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
-        translation *= Time.deltaTime;
-        rotation *= Time.deltaTime;
-        transform.Translate(0, 0, translation);
-        transform.Rotate(0, rotation, 0);
 
-
+        movement();
 
 
         if (Input.GetMouseButton(0))
@@ -60,10 +52,10 @@ public class PlayerControlls : MonoBehaviour
                 {
                     print("shooooooting");
                     timeRemaining = 0;
-                    isBowDrawn = false; 
+                    isBowDrawn = false;
                 }
 
-             
+
 
             }
         }
@@ -71,50 +63,55 @@ public class PlayerControlls : MonoBehaviour
 
         if (Input.GetMouseButtonUp(0))
         {
-          //  print("release");
-            //anim.SetBool("isAiming", false);
-            // anim.SetBool("isAiming", false);
-            print("Shooting"); 
+            shootArrow();
         }
 
-        if (! Input.GetMouseButton(0))
+
+        if (!Input.GetMouseButton(0))
         {
-                anim.SetBool("isAiming", false);
-
-        }
-        
-
-
-
-            if (Input.GetKey(KeyCode.W))
-        {
-
-            speed = 4; 
-            anim.SetTrigger("isRunning");
-            anim.SetBool("isIdle", false);  // WHne attacking, take em out from the current state.
-         
+            anim.SetBool("isAiming", false);
         }
 
-        if (translation != 0)
-        {
-            anim.SetBool("isRunning", true); 
-        }
-        else
-        {
-            anim.SetBool("isRunning", false); 
-        }
+
+
     }
 
 
 
 
 
+    public void movement()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
 
 
-
-        public void shootArrow(int power)
+        if (Input.GetKey(KeyCode.W))
         {
-            // pew pew 
+            speed = 4;
+            anim.SetBool("isIdle", false);  // WHne attacking, take em out from the current state.
+
         }
+
+        if (translation != 0)
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
+    }
+
+
+    public void shootArrow()
+    {
+        print("pew pew");
+        anim.SetBool("isFiring", true);
+    }
 
 }
