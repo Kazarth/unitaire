98fd9919274511d5def9275b68b73201029571cf
Kazaruth <spam.stenfeldt@gmail.com>
2021-05-24 14:16:35 +0200

    Merged torBranch with main.

--- Project/Assets/Barricade_Animation.cs
+++ Project/Assets/Barricade_Animation.cs
@@ -5,13 +5,13 @@ using UnityEngine.UI;
 using System.Threading;
 
 public class Barricade_Animation : MonoBehaviour {
-    public float healthPool = 100;
-    public float currentCollisionCount = 0;
-    private float attackMultiplier = 0;
-    [SerializeField] Slider barricadeHealthSlider;
-    [SerializeField] Canvas gameOverCanvas;
-    [SerializeField] Image image;
-    [SerializeField] GameObject barricadeObject;
+  public float healthPool = 100;
+  public float currentCollisionCount = 0;
+  private float attackMultiplier = 0;
+  [SerializeField] Slider barricadeHealthSlider;
+  [SerializeField] Canvas gameOverCanvas;
+  [SerializeField] Image image;
+  [SerializeField] GameObject barricadeObject;
 	public GameObject effect;
 		
 	public void Start() {
@@ -19,35 +19,35 @@ public class Barricade_Animation : MonoBehaviour {
 	}
 		
 	private void Update() {
-        if (healthPool <= 0) {
+    if (healthPool <= 0) {
 			GameObject[] barricade = GameObject.FindGameObjectsWithTag("Barricade");
 
 			foreach (GameObject go in barricade) {
 				go.SetActive(false);
 			}
 				
-            //Destroy(GameObject.FindWithTag("Barricade"));
-            gameOverCanvas.gameObject.SetActive(true);
-            barricadeObject.SetActive(false);
+      //Destroy(GameObject.FindWithTag("Barricade"));
+      gameOverCanvas.gameObject.SetActive(true);
+      barricadeObject.SetActive(false);
             
-            for(float i = 0; i <= 1f; i += 0.001f){
-                image.color = new Color(1, 1, 1, i);
-                Debug.Log(i);
-            }
+      for(float i = 0; i <= 1f; i += 0.001f){
+        image.color = new Color(1, 1, 1, i);
+        Debug.Log(i);
+      }
 
 			effect.SetActive(true);
-        }
+    }
 	}
 
-    private void OnCollisionEnter(Collision collision) {
-        currentCollisionCount++;
-        attackMultiplier += 0.01f;
-    }
+  private void OnCollisionEnter(Collision collision) {
+    currentCollisionCount++;
+    attackMultiplier += 0.01f;
+  }
 
-    private void OnCollisionStay(Collision collision) {
-        healthPool = healthPool - attackMultiplier;
-        barricadeHealthSlider.value = healthPool;
-    }
+  private void OnCollisionStay(Collision collision) {
+    healthPool = healthPool - attackMultiplier;
+    barricadeHealthSlider.value = healthPool;
+  }
 		
 	private void OnCollisionExit(Collision collision) {
 		currentCollisionCount--;
